30d552b7c94be93dd1403f719f27350562b51432
Tareq Ali <tareqmaher02@gmail.com>
2022-10-10 20:26:29 +0300

    drawer

--- lib/task_managment/main_tasks_screen.dart
+++ lib/task_managment/main_tasks_screen.dart
@@ -12,7 +12,7 @@ class MainTaskScreen extends StatefulWidget {
   State<MainTaskScreen> createState() => _MainTaskScreenState();
 }
 
-class _MainTaskScreenState extends State<MainTaskScreen> {
+class _MainTaskScreenState extends State<MainTaskScreen> with SingleTickerProviderStateMixin {
   checkTask(TaskModel taskModel){
     int index=tasks.indexOf(taskModel);
     print(tasks[index].isComplete);
@@ -20,16 +20,65 @@ class _MainTaskScreenState extends State<MainTaskScreen> {
     print(tasks[index].isComplete);
     setState(() {});
   }
+  late TabController tabController;
+  initializeTabController(){
+    tabController = TabController(length: 3, vsync: this);
+  
+  }
+  @override
+  void initState(){
+    super.initState();
+    initializeTabController();
+  }
+  
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
       length: 3,
       child: Scaffold(
+        drawer: Drawer(
+          child: Column(
+            children: [
+              const UserAccountsDrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.orangeAccent
+                ),
+                currentAccountPicture: CircleAvatar(
+                  backgroundColor: Colors.deepPurpleAccent,
+                  child: Text('T'),
+                ),
+                accountName: Text('Tareq Ali'), 
+                accountEmail: Text('tareqmaher02@gmail.com'),
+                ),
+                ListTile(
+                  title: const Text('ALL TASKS'),
+                  subtitle: const Text('go to all tasks page'),
+                  leading: const Icon(Icons.list),
+                  trailing: const Icon(Icons.arrow_forward_ios),
+                  onTap:(){ tabController.animateTo(0);},
+                ),
+                ListTile(
+                  title: const Text('COMPLETE TASKS'),
+                  subtitle: const Text('go to complete tasks page'),
+                  leading: const Icon(Icons.done),
+                  trailing: const Icon(Icons.arrow_forward_ios),
+                  onTap:(){ tabController.animateTo(1);},
+                ),
+                ListTile(
+                  title: const Text('INCOMPLETE TASKS'),
+                  subtitle: const Text('go to incomplete tasks page'),
+                  leading: const Icon(Icons.cancel),
+                  trailing: const Icon(Icons.arrow_forward_ios),
+                  onTap:(){ tabController.animateTo(2);},
+                ),
+            ],
+          ),
+        ),
       appBar: AppBar(
         backgroundColor: Colors.orangeAccent,
         shadowColor: Colors.amber,
         title: const Text('Task Managment'),
-        bottom: const TabBar(isScrollable: true,unselectedLabelColor: Colors.grey,indicatorColor: Colors.black,tabs: [
+        bottom: TabBar(controller: tabController, isScrollable: true,unselectedLabelColor: Colors.grey,indicatorColor: Colors.black,tabs: const [
           Tab(
             text: 'All Tasks',
           ),
@@ -41,7 +90,7 @@ class _MainTaskScreenState extends State<MainTaskScreen> {
           )
         ]),
       ),
-      body: TabBarView(children: [
+      body: TabBarView(controller: tabController,children: [
         AllTaskScreen(checkTask: checkTask,),
         CompleteTaskScreen(checkTask: checkTask,),
         InCompleteTaskScreen(checkTask: checkTask,),
